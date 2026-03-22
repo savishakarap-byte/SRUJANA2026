92daf153b8a3d1f1690a09d0efffead8556390f7
SavishkarAP <savishakarap@gmail.com>
2026-03-22 21:12:00 +0530

    Update Register.tsx

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -18,7 +18,7 @@ const eventPricing = {
   "Paper presentation": { individual: 200, team: 500 },
   "Poster presentation": { individual: 200, team: 500 },
   "Hackathon": { individual: 200, team: 500 },
-  "Industry institute interaction": { individual: 100 }, // ✅ only individual
+  "Industry institute interaction": { individual: 1 }, // ✅ only individual
 };
 export default function Register() {
 
