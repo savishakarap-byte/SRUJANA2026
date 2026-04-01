eca9084cdeaab09a9b0c0e7465503c3b80d6f279
SavishkarAP <savishakarap@gmail.com>
2026-04-01 19:58:13 +0530

    Update Maintenance.jsx

--- src/pages/Maintenance.jsx
+++ src/pages/Maintenance.jsx
@@ -3,11 +3,11 @@ const Maintenance = () => {
     <div className="min-h-screen flex items-center justify-center bg-slate-900 text-white text-center px-4">
       <div>
         <h1 className="text-4xl font-bold mb-4">
-          🚫 Registrations Start Soon
+          🚫 Registrations Closed 
         </h1>
         <p className="opacity-80">
-          Registrations for SRUJANA2026 are temporarily unavailable.
-          Please check back later.
+          Registrations for SRUJANA2026 are Closed. 
+          Thank you for Showing Interest but it's too late 
         </p>
       </div>
     </div>
